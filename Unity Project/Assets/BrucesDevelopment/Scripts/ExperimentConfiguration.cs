0737854fb40bb0a1efca01be6c8557a3a341bd58
Bruce Weir <bruce.weir@bbc.co.uk>
2019-10-29 14:45:56 +0000

    added ConfigurationReader class to read a configuration file containing the experiment setup. Also added an example config.json file to show how they should be written. Calling the GetExperimentConfiguration method from this class, with the experiment number argument, will return the list of videos to be used in this experiment

---
+++ Unity Project/Assets/BrucesDevelopment/Scripts/ExperimentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+/// <summary>
+/// Class to be read by the UnityEngine.JSONUtility class as used in
+/// the ExperimentConfigurationReader class.
+/// If this is changed then the structure of the matching JSON file
+/// will also need to change to match it.
+/// </summary>
+public class ExperimentConfiguration
+{
+    public int experimentNumber;
+    public string[] videoUrls;
+}
+
+[Serializable]
+public class Experiment
+{
+    public ExperimentConfiguration[] experimentConfigurations;
+}
---
+++ Unity Project/Assets/BrucesDevelopment/Scripts/ExperimentConfigurationReader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+public class ExperimentConfigurationReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Experiment experiment = null;
+
+    /// <summary>
+    /// Returns the requested experiment configuration from the JSON file (default "config.json"),
+    /// which should be in the Persistent storage directory (windows: \user\[name]\Appdata\LocalLow\rd_bbc_co_uk\ResolutionEngagement\)
+    /// </summary>
+    public ExperimentConfiguration GetExperimentConfiguration(int number, string configurationFilename="config.json")
+    {
+        if(!ReadConfigFile(configurationFilename))
+        {
+            return null;
+        }
+
+        else
+        {
+            foreach(ExperimentConfiguration exc in experiment.experimentConfigurations)
+            {
+                if(exc.experimentNumber == number)
+                {
+                    return exc;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    private bool ReadConfigFile(string configurationFilename)
+    {
+        string filePath = Path.Combine(Application.persistentDataPath, configurationFilename);
+
+        if(!File.Exists(filePath))
+        {
+            Debug.Log("Could not find file: " + filePath);
+            return false;
+        }
+
+        string fileContent = File.ReadAllText(filePath);
+
+        try
+        {
+            experiment = FromJson(fileContent);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Json parsing error: " + e.Message);
+            return false;
+        }
+
+        return true;
+    }
+
+    private Experiment FromJson(string json)
+    {
+        return JsonUtility.FromJson<Experiment>(json);
+    }
+
+}
