05da42fb0cc1b99e23f23c0209601ad164639d65
Bruce Weir <bruce.weir@bbc.co.uk>
2019-10-30 11:21:57 +0000

    added example of click detection

---
+++ Unity Project/Assets/BrucesDevelopment/Scripts/QuickTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class QuickTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public DetectClicks detectClicks;
+    void Start()
+    {
+        detectClicks.onClick = ProcessClick;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ProcessClick(float clickTime, PointerEventData.InputButton button)
+    {
+        Debug.Log("ProcessClick: " + clickTime + " " + button);
+    }
+}
