59b9a53ed868a956e30bcf7d0e633a2911cacfe0
pejmansa <psaeghe@gmail.com>
2019-10-30 15:18:41 +0000

    bug fixed

--- Unity Project/Assets/PJDev/Scripts/AudioPlayerScript.cs
+++ Unity Project/Assets/PJDev/Scripts/AudioPlayerScript.cs
@@ -1,18 +1,19 @@
 ﻿//This script allows you to toggle music to play and stop.
 //Assign an AudioSource to a GameObject and attach an Audio Clip in the Audio Source. Attach this script to the GameObject.
 
+using System;
+using System.Timers;
 using UnityEngine;
 
 public class AudioPlayerScript : MonoBehaviour
 {
+
     public int timeBetweenBeepsSeconds;
-    AudioSource lo_pitch, hi_pitch;
-    System.DateTime playbackTime;
-    System.TimeSpan beepDuration;
-    System.TimeSpan timeBetweenBeeps;
+    private static AudioSource lo_pitch, hi_pitch;
+    private float timer;
 
     // toggle=false means sound off, toggle=true means sound on
-    bool lo_toggle, hi_toggle, begin;
+    bool lo_toggle, hi_toggle, _playBeep;
 
     void Start()
     {
@@ -21,90 +22,54 @@ public class AudioPlayerScript : MonoBehaviour
         hi_pitch = GameObject.FindGameObjectWithTag(tag: "Hi_Pitch").GetComponent<AudioSource>();
         lo_toggle = false;
         hi_toggle = false;
-        begin = false;
+        _playBeep = false;
+        timer = 0.0f;
     }
 
 
     void FixedUpdate()
     {
-        timeBetweenBeeps = System.DateTime.Now - playbackTime;
-        //Debug.Log("fixed delta time: " + Time.fixedDeltaTime.ToString());
-        //Debug.Log("delta time: " + Time.deltaTime.ToString());
-        Debug.Log("system time in seconds: " + System.DateTime.Now);
-        Debug.Log("time between beeps: " + timeBetweenBeeps.TotalMilliseconds);
+        timer += Time.fixedUnscaledDeltaTime;
+        //Debug.Log(timer);
 
-        //if (Input.GetKeyDown("space"))
-        //{
-        //    PlayBeep();
-        //}
-       
-        if (lo_toggle == false && hi_toggle == false && timeBetweenBeeps.TotalMilliseconds > timeBetweenBeepsSeconds * 1000)
+        if (Input.GetKeyDown("space"))
         {
-            PlayBeep();
-        }
-            
-
-        else if ((lo_toggle || hi_toggle) && (System.DateTime.Now.Millisecond - playbackTime.Millisecond > 25))
-        {
-            StopBeep();
+            timer = 0.0f;
+            _playBeep = true;
         }
 
-
-        //if (Input.GetKeyDown("space") && toggle==false)
-        //{
-        //    if (Random.value < 0.5)
-        //    {
-        //        lo_pitch.Play();
-        //    }
-        //    else
-        //    {
-        //        hi_Pitch.Play();
-        //    }
-        //    toggle = true;
-        //    Debug.Log("lo_pitch played");
-        //    Debug.Log(toggle);
-        //}
-        //else if (Input.GetKeyDown("space") && toggle==true)
-        //{
-        //    lo_pitch.Stop();
-        //    hi_Pitch.Stop();
-        //    toggle = false;
-        //    Debug.Log("lo_pitch stopped");
-        //    Debug.Log(toggle);
-        //}
+        if (_playBeep)
+        {
+            if (timer > timeBetweenBeepsSeconds)
+            {
+                PlayBeep(0.01f);
+            }
+        }
     }
 
     /// <summary>
     /// Receive the start time from another script (the videoPlayer) and plays a short beep every 15 seconds.
     /// </summary>
     /// <returns></returns>
-    public void PlayBeep()
+    /// 
+    public void PlayBeep(float duration)
     {
-        if (Random.value < 0.5)
+        if (UnityEngine.Random.value < 0.5)
         {
             lo_pitch.Play();
-            lo_toggle = true;
+            Invoke("StopBeep", duration);
         }
         else
         {
             hi_pitch.Play();
-            hi_toggle = true;
+            Invoke("StopBeep", duration);
         }
-        playbackTime = System.DateTime.Now;
+        timer = 0f;
     }
 
     public void StopBeep()
     {
-        if (lo_toggle)
-        {
-            lo_pitch.Stop();
-            lo_toggle = false;
-        }
-        else if (hi_toggle)
-        {
-            hi_pitch.Stop();
-            hi_toggle = false;
-        }
+        lo_pitch.Stop();
+        hi_pitch.Stop();
     }
-   
 }
